008e72c0e7bec821b8a31dcf418f67000b1ebf72
Isaac-Valenzuela <isaacamaya941@gmail.com>
2025-03-04 19:53:41 -0500

    push

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Asegura rutas relativas
+  server: {
+    historyApiFallback: true, // Asegura que las rutas no se rompan en desarrollo
+  },
 });
